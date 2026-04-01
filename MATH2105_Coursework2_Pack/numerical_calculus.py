88ca5497e73a17dcae491513c4d1887057cf9f1a
IdotEXE-313 <mcpegamerxx1@gmail.com>
2026-04-01 18:38:22 +0100

    Question 1 complete (need to check graphs)

--- MATH2105_Coursework2_Pack/numerical_calculus.py
+++ MATH2105_Coursework2_Pack/numerical_calculus.py
@@ -59,7 +59,7 @@ def trapezoidal_integration(a,b,f):
 ### Functions to be edited for the coursework ###
 
 ### QUESTION 1 - derivative approximation
-def approximate_derivative(x: float,h:list[float],nvalues:int,f: Callable[[float],float],d2fdx2: Callable[[float], float],produce_fig:bool):
+def approximate_derivative(x: float,h:np.ndarray,nvalues:int,f: Callable[[float],float],d2fdx2: Callable[[float], float],produce_fig:bool):
 
     """
     Approximates the second derivative of a function f at differing values of 'h' using the centered difference method
@@ -67,7 +67,7 @@ def approximate_derivative(x: float,h:list[float],nvalues:int,f: Callable[[float
     Inputs:
     ----------
     x (float): The point at which we aim to evaluate the second derivative of f at
-    h (list): The range of values of h to use for the centered difference method
+    h (numpy.ndarray): The range of values of h to use for the centered difference method
     nvalues (integer): The number of approximations to make with a range of h values
     f (Callable): The function of which its second derivative will be approximated
     d2fdx2 (Callable): The precise second derivative of the function 'f'
@@ -81,19 +81,22 @@ def approximate_derivative(x: float,h:list[float],nvalues:int,f: Callable[[float
 
     """
 
+    #Populate the array to later fill with approximations
     approx_values = np.zeros(nvalues,)
     error_values = np.zeros(nvalues,)
 
-    approx_values = [centred_difference(x,h,f) for h in h]
-    error_values = np.abs(d2fdx2(x) - [])
+    #Calculate the approximations for each 'h' value, and calculate the absolute difference between the actual second derivative and its approximate values
+    approx_values = centred_difference(x,h,f)
+    error_values = np.abs(d2fdx2(x) - approx_values)
 
-    # Remove the following two lines when you have completed the code
-    approx_values = None
-    error_values  = None
-
-    ### Example of creating a figure object
-    fig = plt.figure()        # This line is required
-    plt.plot([0,1],[2,3])     # Delete this line and replace
+    fig=None
+    #Create figure plot if produce_fig is True       
+    if(produce_fig):
+        fig = plt.figure() 
+        plt.loglog(h, error_values, 'o-')
+        plt.xlabel("h-values")
+        plt.ylabel("error values")
+        plt.show()
 
     return approx_values, error_values, fig
 

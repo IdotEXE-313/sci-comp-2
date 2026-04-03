updated bug in denominator of formula

MATH2105_Coursework2_Pack/main.py
@@ -18,8 +18,9 @@ print(error_values)
 import numpy as np
 import matplotlib.pyplot as plt
 import numerical_calculus as n_cal
-x = 0.25; r = 2.0; nlevels = 1; nvalues = 4
-h = 1.0 / (r**(np.arange(nvalues)))
+x = 0.25; r = 2.0; nlevels = 5; nvalues = 10
+# h = 1.0 / (r**(np.arange(nvalues)))
+h= r**(-(np.arange(nvalues)))
 f = lambda x: np.sin(np.pi*x)
 d2fdx2 = lambda x: -np.pi*np.pi*np.sin(np.pi*x)
 error_values, q2_fig = n_cal.extrapolate_derivative(x,h,nvalues,nlevels,r,f,d2fdx2)

MATH2105_Coursework2_Pack/numerical_calculus.py
@@ -133,7 +133,7 @@ def extrapolate_derivative(x:float,h:np.ndarray,nvalues:int,nlevels:int,r:float,
 
         #Update the extrapolations
         for k in range(0, nvalues - l - 1):
-            current_n[k] = ((r**(2*(l+1)))*current_n[k+1] - current_n[k]) / (r**2*(l+1) - 1)
+            current_n[k] = ((r**(2*(l+1)))*current_n[k+1] - current_n[k]) / (r**(2*(l+1)) - 1)
 
 
     # Plot of errors against h_{k} values